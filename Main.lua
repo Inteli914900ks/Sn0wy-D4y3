1559b9e26f5d83e9b40c76fcdce1f43da26c2060
Inteli914900ks <intel.i9.14900ks@gmail.com>
2025-04-15 11:23:06 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,5 @@
-local p=game.Players.LocalPlayer
+local function c()
+	local p=game.Players.LocalPlayer
 local c=p.Character or p.CharacterAdded:Wait()
 local h,c1=c:WaitForChild("Head"),c:WaitForChild("Torso")
 
@@ -23,3 +24,24 @@ h.Color=Color3.new(1,1,1) h:FindFirstChild("face"):Destroy()
 m(h,"rbxassetid://16727952350","rbxassetid://16727798573",nil,Vector3.new(0,.2,0),Vector3.zero)
 m(h,"rbxassetid://15692041811","rbxassetid://16152347751",nil,Vector3.new(0,.2,0),Vector3.zero)
 m(h,"rbxassetid://16717499857","rbxassetid://16717435268",nil,Vector3.new(0,-.4,0),Vector3.zero)
+end
+
+-- Create a LocalScript under StarterPlayerScripts or StarterCharacterScripts
+
+local player = game.Players.LocalPlayer
+
+-- Function to print "Hi" when the game starts
+local function a()
+	c()
+end
+
+-- Function to print "Hi there" every time the character respawns
+local function b()
+    player.CharacterAdded:Connect(function()
+			c()
+    end)
+end
+
+-- Call the functions
+a()
+b()
